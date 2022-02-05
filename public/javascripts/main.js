dbd917bd07b5033fde397e1de7c7779d7688dd86
Jeffrey <jeff.isabella@gmail.com>
2022-02-05 13:06:21 -0500

    adding new event touchmove for mobile
    adding new event touchmove for mobile

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -142,6 +142,23 @@ function random(min = 0, max = Number.MAX_SAFE_INTEGER) {
   return Math.floor(Math.random() * (max - min) + min);
 } 
 
+canvas.addEventListener('touchmove', function() {
+    var mouse = {};
+
+    mouse.x = event.offsetX;
+    mouse.y = event.offsetY;
+    if(mouse.x === undefined){ // if firefox
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    }
+
+    // Substract offset (so it's centered at 0,0)
+    mouse.x -= center.x;
+    mouse.y -= center.y;
+
+    drawFibonacciSpiral({x:0, y:0}, mouse);
+});
+
 canvas.addEventListener('mousemove', function() {
     var mouse = {};
 
